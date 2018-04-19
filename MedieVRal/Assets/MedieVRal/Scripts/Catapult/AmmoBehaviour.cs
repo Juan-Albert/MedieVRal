6f6d330c98bd40a5abc454f62ce61c11142c4fe5
Juan-Albert <juanmartinezlopez96@gmail.com>
2018-04-19 12:55:28 +0200

    Catapulta Objetivos

--- MedieVRal/Assets/MedieVRal/Scripts/Catapult/AmmoBehaviour.cs
+++ MedieVRal/Assets/MedieVRal/Scripts/Catapult/AmmoBehaviour.cs
@@ -1,21 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AmmoBehaviour : MonoBehaviour {
 
     private Rigidbody myRB;
 
-    public Quaternion orientation;
+    public GameObject particle;
 
-	// Use this for initialization
-	void Start () {
+    public Text score;
+
+
+    void Start () {
         myRB = GetComponent<Rigidbody>();
         myRB.AddForce(-transform.right * 1000f);
     }
 	
-	// Update is called once per frame
+	
 	void Update () {
 		
 	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Muro")
+        {
+            
+            Instantiate(particle, collision.gameObject.transform.position, Quaternion.identity);
+            //score.text = (int.Parse(score.text) + 1).ToString();
+            Destroy(collision.gameObject.transform.parent);
+            Destroy(this.gameObject);
+
+        }
+
+        if (collision.gameObject.tag == "Casa")
+        {
+           
+            Instantiate(particle, collision.gameObject.transform.position, Quaternion.identity);
+            //score.text = (int.Parse(score.text) + 3).ToString();
+            Destroy(collision.gameObject);
+            Destroy(this.gameObject);
+
+        }
+    }
 }
---
+++ MedieVRal/Assets/MedieVRal/Scripts/Catapult/ParticleBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(DestroyPaticle());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator DestroyPaticle()
+    {
+
+        yield return new WaitForSeconds(5);
+        Destroy(this.gameObject);
+
+    }
+}
