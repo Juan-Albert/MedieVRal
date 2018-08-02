1703d9e132f4b1a32d3aefc2a4d3880b67df847c
Juan-Albert <juanmartinezlopez96@gmail.com>
2018-08-02 19:52:08 +0200

    SelectLevel

---
+++ MedieVRal/Assets/MedieVRal/Scripts/SelectLevel/LoadLevel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLevel : MonoBehaviour {
+
+    public int level;
+
+	void Start () {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "MainCamera")
+        {
+            SceneManager.LoadScene(level);
+        }
+    }
+
+}
