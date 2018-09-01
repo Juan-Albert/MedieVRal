4b35753b32dee594746964ff00e8218a92bd3f3b
Juan-Albert <juanmartinezlopez96@gmail.com>
2018-09-01 06:57:20 +0200

    Shield V3

---
+++ MedieVRal/Assets/MedieVRal/Scripts/Shield/ArrowProjectile.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowProjectile : MonoBehaviour {
+
+    private Rigidbody rb;
+
+    private float t = 0.0f;
+    private bool moving = false;
+
+    private Vector3 dir;
+
+    private GameObject player;
+
+    private bool hit = false;
+
+    //Distancia entre una posición y la anterior para detectar colision.
+    private float maxDistance;
+
+    //Componentes para calcular la colisión: la posición actual, la posición anterior y la dirección.  
+    private Vector3 actualPos;
+    private Vector3 lastPos;
+    private Vector3 direction;
+
+    //Objeto que almacena la información de la colisión
+    private RaycastHit info;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        player = GameObject.Find("Camera (eye)");
+        transform.LookAt(player.transform.position);
+    }
+
+    void FixedUpdate()
+    {
+        if(!hit)
+        {
+            rb.velocity = transform.forward * 1;
+
+            t = t + Time.deltaTime;
+            if (t > 50.0f)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+        if(!hit)
+        {
+            lastPos = actualPos;
+            actualPos = transform.position;
+            direction = (actualPos - lastPos);
+            maxDistance = Vector3.Distance(actualPos, lastPos);
+            Debug.DrawRay(lastPos, direction, Color.green, 10f);
+            //Se usa la herramienta Raycast para generar rayos que calculen la colisión con los objetivos dada una distancia máxima.
+            if (Physics.Raycast(lastPos, direction, out info, maxDistance))
+            {
+                Debug.DrawRay(lastPos, direction, Color.red, 10f);
+                //Se comprueba si se ha colisionado con una diana.
+
+                if (info.collider.gameObject.tag == "MainCamera")
+                {
+                    Debug.DrawRay(lastPos, direction, Color.blue, 10f);
+                    Destroy(this.gameObject);
+                }
+                else if (info.collider.gameObject.tag == "Shield")
+                {
+                    Debug.DrawRay(lastPos, direction, Color.yellow, 10f);
+                    hit = true;
+                    transform.parent = info.collider.gameObject.transform.parent;
+                    Destroy(rb);
+                    transform.position = info.point;
+                    Debug.Log("Exito");
+
+                }
+
+            }
+        }
+        
+
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "MainCamera")
+        {
+            Destroy(this.gameObject);
+        }
+        else if (other.gameObject.tag == "Shield")
+        {
+            hit = true;
+            Destroy(rb);
+            this.transform.SetParent(other.gameObject.transform.parent);
+            Debug.Log("Exito");
+
+        }
+    }
+
+
+}
---
+++ MedieVRal/Assets/MedieVRal/Scripts/Shield/ShieldManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShieldManager : MonoBehaviour {
+
+    [Tooltip("Objeto donde se va a almacenar la puntuación del jugador.")]
+    public Text scoreUI;
+    public Transform[] arrowSpawns;
+    public GameObject arrow;
+    public GameObject particle;
+
+    private int n_targets = 5;
+    private int targetsDestroyed = 0;
+    private int score = 0;
+
+    
+
+    private void Start()
+    {
+        StartCoroutine(SpawnArrows());
+        
+    }
+
+    private void Update()
+    {
+        if (targetsDestroyed == n_targets)
+        {
+            GameManager.instance.CheckScore(5, score);
+        }
+
+    }
+
+    public void IncreasePoints()
+    {
+
+        scoreUI.text = (int.Parse(scoreUI.text) + 1).ToString();
+        score++;
+    }
+
+    public void Target_Destroyed()
+    {
+        targetsDestroyed++;
+    }
+
+    IEnumerator SpawnArrows()
+    {
+        foreach (Transform spawn in arrowSpawns)
+        {
+            GameObject warning = Instantiate(particle, spawn.transform.position, spawn.transform.rotation) as GameObject;
+            yield return new WaitForSeconds(3);         
+            Destroy(warning);
+            Instantiate(arrow, spawn.transform.position, spawn.transform.rotation);
+            
+        }
+        
+    }
+}
